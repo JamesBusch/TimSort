3a4c80a001a97d4a1b0139c05ff89e3e0eef7f91
JamesCodes <buschj@uoguelph.ca>
2020-01-05 12:34:56 -0500

    Added header comment

--- insertionSort.c
+++ insertionSort.c
@@ -1,3 +1,15 @@
+/**************************
+ * James Busch
+ * 01/05/20
+ * 
+ * Version: 0.01
+ * 
+ * Insertion sort is used in TimSort so I put it in its own file
+ * just to make everything a little bit cleaner
+ * 
+ * ***********************/
+
+
 #include "insertionSort.h"
 #include <stdio.h>
 void insertionSort(int **arr, int size){
