f2dc8197972dd3a156d427db219fd4aaf2196f16
James Busch <buschj@uoguelph.ca>
2020-01-06 13:34:40 -0500

    added header file

---
+++ mergeSort.h
@@ -0,0 +1,15 @@
+/**************************
+ * James Busch
+ * 01/06/20
+ * 
+ * Version: 0.01
+ * 
+ * This is the header file for mergeSort
+ * 
+ * ***********************/
+#ifndef MERGE_SORT_H_
+#define MERGE_SORT_H_
+
+void mergeSort(int **arr, int size);
+
+#endif
