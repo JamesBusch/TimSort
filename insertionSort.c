97d2943954795641315a19ff3f651b8472a07068
James Busch <buschj@uoguelph.ca>
2020-01-06 13:33:02 -0500

    Added function comment

--- insertionSort.c
+++ insertionSort.c
@@ -2,7 +2,7 @@
  * James Busch
  * 01/05/20
  * 
- * Version: 0.01
+ * Version: 0.02
  * 
  * Insertion sort is used in TimSort so I put it in its own file
  * just to make everything a little bit cleaner
@@ -12,11 +12,21 @@
 
 #include "insertionSort.h"
 #include <stdio.h>
+
+/******************
+ * insertionSort
+ * 
+ * This function takes in an array pointer and
+ * the array size then proceds to use insertion sort
+ * to sort the array.
+ * 
+ * In: int **arr, int size
+ * Out: void
+ ******************/
 void insertionSort(int **arr, int size){
     int index;
     int backIndex;
     int holder;
-    //int comparision;
     for(index = 1; index < size; index++){
         holder = (*arr)[index];
         backIndex = index - 1;
