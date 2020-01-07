a336f2ef7e664ca39bb7d82e794d93768a602f93
JamesCodes <buschj@uoguelph.ca>
2020-01-07 16:51:44 -0500

    created header file for new util file

---
+++ includes/utils.h
@@ -0,0 +1,18 @@
+/**************************
+ * James Busch
+ * 01/07/20
+ * 
+ * Version: 0.01
+ * 
+ * This has all of the function definitions for the utils file
+ * 
+ * ***********************/
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+void printArray(int **arr, int length);
+void fillArrayRandom(int **arr, int length, int floor, int roof);
+void fillArrayDescending(int **arr, int length);
+
+#endif
