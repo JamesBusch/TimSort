30bf079305e9df1f95de5f3e10701d7ff61b5fc4
James Busch <buschj@uoguelph.ca>
2020-01-08 12:46:32 -0500

    Fixed date formate in header comment

--- includes/insertionSort.h
+++ includes/insertionSort.h
@@ -1,3 +1,13 @@
+/**************************
+ * James Busch
+ * 01/07/20
+ * 
+ * Version: 0.01
+ * 
+ * This has all of the function definitions for the insertionSort file
+ * 
+ * ***********************/
+
 #ifndef INSERT_SORT_H_
 #define INSERT_SORT_H_
 
