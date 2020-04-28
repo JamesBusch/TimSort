c716445217d9f24108f1611e1c1fdfaf4a15bfd5
JamesCodes <buschj@uoguelph.ca>
2020-04-28 10:04:53 -0400

    added mergeSortTesting

---
+++ includes/mergeSortTest.h
@@ -0,0 +1,15 @@
+/**************************
+ * James Busch
+ * 01/08/20
+ * 
+ * Version: 0.01
+ * 
+ * This is the header file for mergerSortTest that includes the function definitions
+ **************************/
+
+#ifndef MERGE_SORT_TEST_H_
+#define MERGE_SORT_TEST_H_
+
+void mergeSortTest();
+
+#endif
---
+++ src/mergeSortTest.c
@@ -0,0 +1,83 @@
+/**************************
+ * James Busch
+ * 25/04/20
+ * 
+ * Version: 0.01
+ * 
+ * This is the file for all the mergeSort testing
+ **************************/
+
+#include "mergeSort.h"
+#include "mergeSortTest.h"
+#include "arrUtils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void mergeSortTest(){
+    int *arr;
+    int *bigArr;
+    int *smallArr;
+    arr = malloc(sizeof(int) * 10);
+
+    //For both merge sort and insertion sort I will not be testing for invalid arrays
+    //due to both of these being internal implementions never to be seen by the user it
+    //will be up to tim sort to make sure it is passing the functions valid arrays
+    printf("*************************MERGE SORT TEST***************\n");
+    printf("NOTE: only arrays with size <= 10 will have before and after print outs\n");
+
+    printf("========================TEST ONE========================\n");
+    fillArrayDescending(&arr, 10);
+    
+    printf("Orginal Array:\n");
+    printArray(&arr, 10);
+
+    mergeSort(&arr, 0, 9);
+    printf("After insertion sort:\n");
+    printArray(&arr, 10);
+
+    for(int i = 0; i < 10; i++){
+        assert((i + 1) == arr[i]);
+    }
+    printf("=========Assertion succesful, end of test one===========\n");
+
+    printf("========================TEST TWO========================\n");
+    fillArrayRandom(&arr, 10, -500, 500);
+
+    printf("Orginal Array:\n");
+    printArray(&arr, 10);
+
+    mergeSort(&arr, 0, 9);
+    printf("After insertion sort:\n");
+    printArray(&arr, 10);
+
+    for(int i = 0; i < 9; i++){
+        assert(arr[i] <= arr[i + 1]);
+    }
+    printf("=======Assertion succesful, end of test two=============\n");
+    free(arr);
+
+    printf("=======================TEST THREE=======================\n");
+    bigArr = malloc(sizeof(int) * 500);
+    fillArrayRandom(&bigArr, 500, -10000, 10000);
+    mergeSort(&bigArr, 0, 499);
+    for(int i = 0; i < 499; i++){
+        assert(bigArr[i] <= bigArr[i + 1]);
+    }
+    printf("=====Assertion succesful, end of test three=============\n");
+    free(bigArr);
+
+    printf("=======================TEST FOUR========================\n");
+    smallArr = malloc(sizeof(int));
+    smallArr[0] = -1;
+    printf("Original Array:\n");
+    printArray(&smallArr, 1);
+
+    mergeSort(&smallArr, 0, 0);
+    printf("After insertion sort:\n");
+    printArray(&smallArr, 1);
+
+    assert(smallArr[0] == -1);
+    printf("======Assertion succesful, end of test four=============\n");
+    free(smallArr);
+}
